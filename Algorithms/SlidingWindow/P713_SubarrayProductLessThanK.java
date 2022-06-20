484985b0141df3eb64db176b02c30169456647b4
konaer <dejingk@gmail.com>
2022-06-20 17:59:52 -0400

    update

---
+++ Algorithms/SlidingWindow/P713_SubarrayProductLessThanK.java
@@ -0,0 +1,20 @@
+package SlidingWindow;
+
+public class P713_SubarrayProductLessThanK {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        
+        int l = 0, r = 0, ans = 0;
+        int product = 1;
+        while (r < nums.length) {
+            product *= nums[r++];
+            while (product >= k) {
+                product /= nums[l++];
+            }
+            ans += r - l;
+        }
+        return ans;
+    }
+}
