5cc6e7be2e4004c808af1044f4017eb6c302569f
konaer <dejingk@gmail.com>
2022-07-14 01:38:10 -0400

    update

---
+++ Algorithms/DataStructure/Tree/P366_FindLeavesOfBinaryTree.java
@@ -0,0 +1,45 @@
+package DataStructure.Tree;
+
+import java.util.*;
+
+public class P366_FindLeavesOfBinaryTree {
+    List<List<Integer>> ans = new ArrayList<>();
+    
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        findHeight(root);
+        return ans;
+    }
+    
+    private int findHeight(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        int left = findHeight(root.left);
+        int right = findHeight(root.right);
+        int cur = Math.max(left, right) + 1;
+        if (ans.size() == cur) {
+            ans.add(new ArrayList<>());
+        }
+        ans.get(cur).add(root.val);
+        return cur;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
